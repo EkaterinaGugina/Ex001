5973d0606efd58844c9e98cfb5b5ae6a85bfafcc
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-16 11:37:48 +0500

    update file Method2

--- Method2/Program.cs
+++ Method2/Program.cs
@@ -4,7 +4,7 @@ void Method2(string msg)
     Console.WriteLine(msg); 
 }
 Method2("Мой дядя самых честных правил");
-
+Console.WriteLine(); 
 
 void Method21(string msg, int count)
 {
@@ -14,5 +14,9 @@ while (i < count)
     Console.WriteLine(msg); 
     i++;
 }
+Console.WriteLine(); 
 }
 Method21("Самый лучший день  ", 4);
+
+Method21(msg: "Какой хороший пень  ", count: 3);
+//Явно указывая наименование аргумента, не обязательно писать их по порядку.
