cc96db78433191cf2332429bc34273e066945d1d
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-16 14:33:00 +0500

    add Ex4_sortirovka

--- Ex4_sorting_array/Program.cs
+++ Ex4_sorting_array/Program.cs
@@ -1,2 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)   // метод вывода на экран массива
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}  ");
+    }
+    Console.WriteLine();
+}
+PrintArray(arr);
+
+void SelectionSort_vozr(int[] array)         // Метод сортировки по возрастанию
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+            {
+                if(array[j] < array[minPosition]) 
+                {   
+                    minPosition = j;
+                }
+            }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+SelectionSort_vozr(arr);
+PrintArray(arr);
+
+void SelectionSort_ubav(int[] array)         // Метод сортировки по убыванию
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i+1; j < array.Length; j++)
+            {
+                if(array[j] > array[maxPosition]) 
+                {   
+                    maxPosition = j;
+                }
+            }
+        int current = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = current;
+    }
+}
+SelectionSort_ubav(arr);
+PrintArray(arr);
