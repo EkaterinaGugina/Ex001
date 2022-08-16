c9d415333a606e10d7b5087eba188f8630a31625
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-16 12:08:06 +0500

    created file  Method3

--- Method4/Program.cs
+++ Method4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Method4 - пример метода, который на входе принимает аргументы, и возвращает некоторый аргумент.
+
+//Возвращать будем строку string, передавать будем int. Мы будем string компоновать друг за другом count раз.
+string Method4(int count, string text)
+{
+int i = 0;
+string result = String.Empty;
+while (i < count)
+    {
+    result = result + text;
+    i++;
+    }
+return result;
+}
+
+string res = Method4(6, "С добрым утром! ");
+Console.Write(res);
