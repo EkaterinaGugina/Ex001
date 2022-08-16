639b3f6449f60cd83961ae3354c6ddd3c6c9da6a
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-16 11:05:21 +0500

    add folder Method2

---
+++ Method2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Method2 - метод, который на входе принимает аргументы, но ничего не возвращает.
+void Method2(string msg) 
+{
+    Console.WriteLine(msg); 
+}
+Method2("Мой дядя самых честных правил");
+
+
+void Method21(string msg, int count)
+{
+int i = 0;
+while (i < count)
+{
+    Console.WriteLine(msg); 
+    i++;
+}
+}
+Method21("Самый лучший день  ", 4);
