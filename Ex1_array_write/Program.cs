2fe77b7b6518c5fd3200bd7b86fe5917e325d1bb
EkaterinaGugina <109799129+EkaterinaGugina@users.noreply.github.com>
2022-08-16 12:22:58 +0500

    Ex1 solved

--- Ex1_array_write/Program.cs
+++ Ex1_array_write/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex1 - пример применения цикла for для вывода на экран массива внутри метода
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+void PrintArray(int[] array)
+{
+int count = array.Length;
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"{array[i]}  ");
+}
+Console.WriteLine();
+}
+PrintArray(arr);
